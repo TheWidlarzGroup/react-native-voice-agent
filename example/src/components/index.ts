3b03c2e2cd716af72a22cd8705fccc7a50b01164
Pieczasz <bartekp854@gmail.com>
2025-09-01 19:44:14 +0200

    refactor: change example

--- example/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { BasicExample } from './BasicExample';
-export { AdvancedExample } from './AdvancedExample';
-export { CustomExample } from './CustomExample';
-export { OnlineExample } from './OnlineExample';
